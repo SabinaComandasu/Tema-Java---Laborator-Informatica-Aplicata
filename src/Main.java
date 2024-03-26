a71d87509214ae821f2df547de5f14c04423ebe6
SabinaComandasu <160462398+SabinaComandasu@users.noreply.github.com>
2024-03-26 21:10:09 +0200

    Delete src directory

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-//HomeworkJava-1.3. Suprascrieti toString() dintr-o clasa astfel incat sa returneze
-//toate atributele clasei, intr-un format custom.
-
-public class Main {
-    public static void main(String[] args) {
-        MyClass object = new MyClass("Value1", 10);
-        System.out.println("\nAtributele obiectului creat sunt:\n" + object.toString());
-    }
-}
