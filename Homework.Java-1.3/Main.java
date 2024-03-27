4fc4c27446eb9eaef3be6d7134bc420bf4089951
SabinaComandasu <160462398+SabinaComandasu@users.noreply.github.com>
2024-03-27 19:52:16 +0200

    Add files via upload

---
+++ Homework.Java-1.3/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        MyClass myObject = new MyClass(" Valoare de test ", 10);
+        System.out.println("\nAtributele obiectului creat sunt:\n" + myObject.toString());
+    }
+}
